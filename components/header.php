41d2237a2d77154a25bfa0bdb6912c387cd86668
Izy-Blinken <m.sharmainsalamat@gmail.com>
2025-12-20 15:24:48 +0800

    I commented sa loob ng index.html ung changes ko

--- components/header.php
+++
@@ -1,3 +0,0 @@
-<div class="logo">
-    <h1 class="brand-name">HOMME D'OR</h1>
-</div>
--- test_header.php
+++ test_header.php
@@ -1,6 +1,8 @@
 <?php
+
 include 'backend/db_connect.php';
-echo "project: 0 tulog\n trabaho: tambay";
+
 ?>
+
 
 //to make sure db is working
