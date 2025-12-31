92fbb0239790e2673b5bd4059cfee710cde2bdc1
Izy-Blinken <m.sharmainsalamat@gmail.com>
2025-12-31 08:08:47 +0800

    blog page done

--- pages/blog.php
+++ pages/blog.php
@@ -9,6 +9,127 @@
     <body>
         <?php include '../components/header.php'; ?>
         <main>
+            <section id = "blog-header"> 
+                <img src="../assets/images/brand_images/collab_image.png" alt="Blog Header Image" width = "500px" height = "500px">
+            <div>
+                <h2>Collaboration</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                <br><br>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Curabitur pretium tincidunt lacus. Nulla gravida orci a odio, at fermentum massa tempor nec. Integer nec odio praesent libero, sed cursus ante dapibus diam. Sed nisi nulla quis sem at nibh elementum imperdiet.</p>
+            </div>
+
+            </section>
+
+            <section id = "top-comments">
+                <div>
+                    <h2>Customers' Top Comments</h2>
+                    
+                </div>
+                <div class = "comments-container">
+                    <div class="comment-box">
+                        <img src="../assets/images/products_images/customerPic.png" alt="User Avatar">
+                        <div class="comment-text">
+                            <h4>Username</h4>
+                            <p>Comment text here...</p>
+                        </div>
+                    </div>
+
+                    <div class="comment-box">
+                        <img src="../assets/images/products_images/customerPic.png" alt="User Avatar">
+                        <div class="comment-text">
+                            <h4>Username</h4>
+                            <p>Comment text here...</p>
+                        </div>
+                    </div>
+
+                    <div class="comment-box center">
+                        <img src="../assets/images/products_images/customerPic.png" alt="User">
+                        <div class="comment-text">
+                            <h4>Username</h4>
+                            <p>Comment text here...</p>
+                        </div>
+                    </div>
+
+                    <div class="comment-box">
+                        <img src="../assets/images/products_images/customerPic.png" alt="User Avatar">
+                        <div class="comment-text">
+                            <h4>Username</h4>
+                            <p>Comment text here...</p>
+                        </div>
+                    </div>
+
+                    <div class="comment-box">
+                        <img src="../assets/images/products_images/customerPic.png" alt="User Avatar">
+                        <div class="comment-text">
+                            <h4>Username</h4>
+                            <p>Comment text here...</p>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+
+            <section id="highlights">
+
+                <div>
+                <h2>In Focus</h2>
+                </div>
+
+                <div class="highlights-grid">
+                    <div class="highlight-item">
+                        <img src="../assets/images/products_images/customerPic.png" alt="Highlight 1">
+                    </div>
+                    <div class="highlight-item">
+                        <img src="../assets/images/products_images/customerPic.png" alt="Highlight 2">
+                    </div>
+                    <div class="highlight-item">
+                        <img src="../assets/images/products_images/nocturne.png" alt="Highlight 3">
+                    </div>
+                    <div class="highlight-item">
+                        <img src="../assets/images/products_images/nocturne.png" alt="Highlight 4">
+                    </div>
+                    <div class="highlight-item">
+                        <img src="../assets/images/products_images/customerPic.png" alt="Highlight 5">
+                    </div>
+                    <div class="highlight-item">
+                        <img src="../assets/images/products_images/customerPic.png" alt="Highlight 6">
+                    </div>
+                </div>
+                
+                <div class="description">
+                    <h3>Read</h3>
+                    <div class="description-text">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                        
+                    </div>
+                </div>
+            </section>
+
+            <section id="ads-section">
+    
+                <div class="ads-container">
+                    <div class="ad-box">
+                        <img src="../assets/images/products_images/ad1.png" alt="Advertisement 1">
+                    </div>
+                    <div class="ad-box">
+                        <img src="../assets/images/products_images/ad2.png" alt="Advertisement 2">
+                    </div>
+                </div>
+                
+               
+                <div class="content-container">
+                    <div class="content-item">
+                        <h3>Holiday gifts selection</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Item in bonitat vel, nostrum erat sed, sagittis et metus gravida, blandiquet a lorem.</p>
+                        <button>DISCOVER</button>
+                    </div>
+                    
+                    <div class="content-item">
+                        <h3>Holiday gifts selection</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Item in bonitat vel, nostrum erat sed, sagittis et metus gravida, blandiquet a lorem.</p>
+                        <button>DISCOVER</button>
+                    </div>
+                </div>
+            </section>
 
         </main>
 
