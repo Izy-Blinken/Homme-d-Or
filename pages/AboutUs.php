e735a2a6ca0d207e7f36cb768add101c8871f581
Izy-Blinken <m.sharmainsalamat@gmail.com>
2026-01-02 19:04:46 +0800

    creating About Us page

--- pages/AboutUs.php
+++ pages/AboutUs.php
@@ -11,6 +11,20 @@
         <?php include '../components/header.php'; ?>
         <main>
 
+        <div class="aboutUs-container">
+            <div class="aboutUs-text">
+            <h3>Our Story</h3>
+            <p>Founded in 2020, Homme d'Or has been dedicated to providing high-quality
+            products and exceptional service to our customers. Founded in 2020, Homme d'Or has been dedicated to providing high-quality
+            products and exceptional service to our customers. Founded in 2020, Homme d'Or has been dedicated to providing high-quality
+            products and exceptional service to our customers. Founded in 2020, Homme d'Or has been dedicated to providing high-quality
+            products and exceptional service to our customers. Founded in 2020, Homme d'Or has been dedicated to providing high-quality
+            products and exceptional service to our customers.</p>
+            </div>
+
+            <img src="../assets/images/brand_images/collab_image.png" alt = "Our Story Image">
+
+        </div>
         </main>
 
         <?php include '../components/footer.php'; ?>
