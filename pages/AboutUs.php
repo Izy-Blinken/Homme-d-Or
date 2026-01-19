239f9c5778fee41aecd927f8efbe31bad17a5226
Izy-Blinken <m.sharmainsalamat@gmail.com>
2026-01-20 02:11:29 +0800

    about us page done

--- pages/AboutUs.php
+++ pages/AboutUs.php
@@ -43,6 +43,53 @@
 
         </div>
 
+        <div class="authors">
+            <div class="authTitle">
+                <h3>Authors</h3>
+
+            </div>
+
+            <div class="authContainer">
+                <div class="authBox"></div>
+                <div class="authBox"></div>
+                <div class="authBox"></div>
+                <div class="authBox"></div>
+            </div>
+
+            <div class="authDescription">
+                <h3>About the Authors</h3>
+                <div class="description-text">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                        
+                </div>
+            </div>
+
+        </div>
+        <section id="highlights">
+        <div class="ads-container">
+            <div class="ad-box">
+                <img src="../assets/images/products_images/ad1.png" alt="Advertisement 1">
+            </div>
+            <div class="ad-box">
+                <img src="../assets/images/products_images/ad2.png" alt="Advertisement 2">
+            </div>
+        </div>
+                
+        <div class="content-container">
+            <div class="content-item">
+                <h3>Holiday gifts selection</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Item in bonitat vel, nostrum erat sed, sagittis et metus gravida, blandiquet a lorem.</p>
+                <button>DISCOVER</button>
+            </div>
+                    
+            <div class="content-item">
+                <h3>Holiday gifts selection</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Item in bonitat vel, nostrum erat sed, sagittis et metus gravida, blandiquet a lorem.</p>
+                <button>DISCOVER</button>
+            </div>
+        </div>
+        </section>
+
         </main>
 
         <?php include '../components/footer.php'; ?>
